83e8f8381fb23b548cb9bd71daff043c0064fd55
ccalobeto <calobeto.leon@gmail.com>
2023-09-03 15:56:32 -0500

    metrics
    append mistakes
    metrics per period
    correct event_places

--- src/nb/missed.R
+++ src/nb/missed.R
@@ -14,6 +14,16 @@ label_duplicated_entry <- function(df, columns) {
     mutate(numbering = row_number()) %>%
     mutate(error_label = ifelse(numbering > 1, "duplicated_entry", error_label))
 }
+
+calculate_missed_averages <- function(df, columns, avg_period) {
+  df %>%
+    filter(missing_state == 1) %>%
+    count(pick({{ columns }})) %>%
+    select(year, n) %>%
+    group_by(year) %>%
+    summarise("{{ avg_period }}" = mean(n))
+}
+
 df <- df_[order(df_$name,
                 df_$event_date,
                 desc(df_$missing_state)), ] %>%
@@ -21,7 +31,6 @@ df <- df_[order(df_$name,
   mutate(cum_sum = cumsum(missing_state))
 # label missing data
 df[df$name == "", "error_label"] <- "no name"
-df[df$event_place == "", "error_label"] <- "no place"
 # label duplicated entries
 df <- df %>%
   label_duplicated_entry(c(name, cum_sum, missing_state))
@@ -35,7 +44,8 @@ df <- df %>%
   ungroup() %>%
   filter(is.na(error_label)) %>%
   select(name, event_date, event_place, missing_state)
-# duplicated entries shifting the missing_state and calculate the difference
+# detecting another duplicated entries: shifting the missing_state
+# and calculate the difference
 df <- df %>%
   group_by(name) %>%
   mutate(diff = missing_state - lag(missing_state, default = NULL))
@@ -48,19 +58,52 @@ df <- df %>%
   ungroup() %>%
   filter(is.na(error_label)) %>%
   select(name, event_date, event_place, missing_state)
+mistakes <- rbind(mistakes, more_mistakes)
+# detect addresses with "", only numbers and "."
+df$event_place_clean <- df$event_place
+df[df$event_place_clean %in% c("", NA), "event_place_clean"] <- NA
+df[grepl("^\\d+$", df$event_place_clean), "event_place_clean"] <- NA
+df[grepl("^\\.$", df$event_place_clean), "event_place_clean"] <- NA
+# calculating period variables
+df$event_date <- as.Date(df$event_date)
+df$year <- year(df$event_date)
+df$month <- month(df$event_date)
+df$day <- day(df$event_date)
+# adding identifier per name
+df <- df %>%
+  group_by(name) %>%
+  mutate(ID = cur_group_id()) %>%
+  ungroup()
+# metrics
+avg_day <- df %>% calculate_missed_averages(c(year, month, day), avg_mpd)
+avg_month <- df %>% calculate_missed_averages(c(year, month), avg_mpm)
+sum_year <- df %>%
+  filter(missing_state == 1) %>%
+  group_by(year) %>%
+  count(missing_state) %>%
+  ungroup() %>%
+  rename(mpy = n) %>%
+  select(year, mpy)
+list_df <- list(avg_day, avg_month, sum_year)
+metrics <- list_df %>% reduce(inner_join, by = "year")
+
 # convert long to pivot data
 df0 <- df[df$missing_state == 0, ]
 df1 <- df[df$missing_state == 1, ]
 output <- merge(x = df1, y = df0, by = "name", all.x = TRUE)
 # test
-write.csv(df, "output.csv", row.names = FALSE)
-
+write.csv(df, "data/out/output.csv", row.names = FALSE)
+write.csv(mistakes, "data/out/mistakes.csv", row.names = FALSE)
 
 # testing
 duplicates <- df_ %>%
   group_by(name) %>%
   summarise(n = n()) %>%
   filter(n > 1)
+
+lapply(df_ %>% filter(name == "SORIA LOPEZ DAVID DEYSON"), unlist)
+
+df %>% filter(name == "VILLEGAS TUIRO ROEL ALEJANDRO")
 
 # get coordinates
 coordinates <- get_google_coordinates(df[1:20, 5])
